1d318bcbaee92ff7280ca4706c1eefb5ffa28ed3
ReidHarbin <harbinreidt@gmail.com>
2023-04-06 21:00:39 -0400

    Finished tasks for MT1 so that tests are passing.

--- src/com/amazon/ata/music/playlist/service/converters/ModelConverter.java
+++ src/com/amazon/ata/music/playlist/service/converters/ModelConverter.java
@@ -3,6 +3,10 @@ package com.amazon.ata.music.playlist.service.converters;
 import com.amazon.ata.music.playlist.service.models.PlaylistModel;
 import com.amazon.ata.music.playlist.service.dynamodb.models.Playlist;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class ModelConverter {
     /**
      * Converts a provided {@link Playlist} into a {@link PlaylistModel} representation.
@@ -10,8 +14,21 @@ public class ModelConverter {
      * @return the converted playlist
      */
     public PlaylistModel toPlaylistModel(Playlist playlist) {
+        List<String> tagList = new ArrayList<>();
+        Set<String> tagSet = playlist.getTags();
+        if (tagSet.isEmpty()) {
+            tagList = null;
+        } else {
+            for (String tag : tagSet) {
+                tagList.add(tag);
+            }
+        }
         return PlaylistModel.builder()
-            .withId(playlist.getId())
-            .build();
+                .withId(playlist.getId())
+                .withName(playlist.getName())
+                .withCustomerId(playlist.getCustomerId())
+                .withSongCount(playlist.getSongCount())
+                .withTags(tagList)
+                .build();
     }
 }
